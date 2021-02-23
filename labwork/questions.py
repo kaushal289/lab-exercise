64130ef11756db7eecf6cd5462a3f29ddafa2ed1
kaushal khanal <khanalkaushal9@gmail.com>
2021-02-23 11:24:36 +0545

    addition

---
+++ labwork/questions.py
@@ -0,0 +1,7 @@
+'''Write a program that takes three numbers
+and print their sum. Every number is given on a seperate line'''
+a=int(input('enter first number:'))
+b=int(input('enter second number:'))
+c=int(input('enter third number:'))
+d=a+b+c
+print('the sum is=',d)
