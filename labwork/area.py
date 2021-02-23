635e004441e9c3cbb95656809a0c98f5af2bc67c
kaushal khanal <khanalkaushal9@gmail.com>
2021-02-23 11:36:20 +0545

    area

---
+++ labwork/area.py
@@ -0,0 +1,6 @@
+'''write a program that reads the length of the base and the height of a right-angled
+triangle and prints the area.  Every number is given on a separate line'''
+b=int(input('enter a length of base:'))
+h=int(input('enter a length of height:'))
+a=(b*h)//2
+print(f'the area is {a}')
