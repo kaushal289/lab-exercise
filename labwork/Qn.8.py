8987b21cec02eaf11f736bbaede4b2cd8ca4b0e7
kaushal khanal <khanalkaushal9@gmail.com>
2021-03-09 12:09:21 +0545

    initial commit

---
+++ labwork/Qn.8.py
@@ -0,0 +1,6 @@
+'''Write a Python program which accepts the radius of a circle from the user and compute the area.(area of
+circle=PI*r^2)'''
+radius=(float(input("enter the radius of circle")))
+PI=22/7
+Area_of_circle=PI*(radius**2)
+print('the area of circle is :',Area_of_circle)
