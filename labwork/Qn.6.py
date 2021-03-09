c3872e60a3b134ae02f58a51cff3ee4a32cfc654
kaushal khanal <khanalkaushal9@gmail.com>
2021-03-09 11:35:22 +0545

    question3

---
+++ labwork/Qn.6.py
@@ -0,0 +1,9 @@
+'''6. Solve each of the following problems using python Script. Make sure you  use  appropriate variable name
+ and comments. When there is a final answer have Python print it to the screen.
+ A person's body mass index(BM) is defined as:
+ BMI= mass in kg/(height in m)^2.'''
+mass=float(input('enter the mass of person in kg:'))
+height=float(input('enter the height of person in m :'))
+BMI=(mass/height**2)
+print("The BIM index of a person is",BMI)
+
