a18bd61b75f1e53e662364bd4d4a0c1896741b20
kaushal khanal <khanalkaushal9@gmail.com>
2021-03-09 12:14:56 +0545

    initial commit

---
+++ labwork/Qn.9.py
@@ -0,0 +1,5 @@
+'''Write a python program to find sum of the first n positive integers.
+sum=(n*(n+1))/2'''
+number=int(input("enter the positive integer "))
+sum=(number*(number+1))/2
+print('sum of the first n positive integer = ',sum)
