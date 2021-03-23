fa77757060e084a28b248a8b58a2ff5866f7ee58
kaushal khanal <khanalkaushal9@gmail.com>
2021-03-23 12:06:13 +0545

    question1

---
+++ labwork2/question_1.py
@@ -0,0 +1,7 @@
+'''1) check whether 5 is in list of first 5 natural numbers or not.Hint: List=>[1,2,3,4,5]'''
+n=5
+list=[1,2,3,4,5]
+if n in list:
+    print("5 is in the list")
+else:
+    print('5 is not in the list')
